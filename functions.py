9e06044493a2ba988c16ef91f68136d7c3a63fa5
IliaMaks <87599530+IliaMaks@users.noreply.github.com>
2025-03-21 10:15:18 +0200

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,12 @@
+def AddIncome (data):
+      print ("AddIncome")
+      return data
+def AddExpense(data) :
+      print ("AddExpense")
+      return data
+def ShowBalance (data):
+      print ("ShowBalance")
+      return data
+def ShowTransactionHistory (data):
+      print ("ShowTransactionHistory")
+      return data
